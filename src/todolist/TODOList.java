334f35b1e07995a8b7b251821efb5f35af6156fa
Ricardo <Ricardo@193.136.237.187>
2016-11-22 23:30:38 +0000

    new commit

--- src/todolist/TODOList.java
+++ src/todolist/TODOList.java
@@ -17,6 +17,7 @@ public class TODOList {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Olá Mundo");
+        System.out.println("asd");
     }
     
 }
