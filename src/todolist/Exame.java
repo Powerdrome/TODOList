5507f5f1d8f32eb1a92fe580cbc0ec474568e8a5
JorgeCecilio <jorgetrovisco@10.65.129.97>
2016-11-25 17:26:32 +0000

    Class Exame

--- src/todolist/Exame.java
+++ src/todolist/Exame.java
@@ -1,5 +1,42 @@
 package todolist;
 
+import java.util.Calendar;
+
 public class Exame {
+    Calendar inicio;
+    Calendar fim;
+    String epoca;
+
+    public Exame(Calendar inicio, Calendar fim, String epoca) {
+        this.inicio = inicio;
+        this.fim = fim;
+        this.epoca = epoca;
+    }
+
+    public void setInicio(Calendar inicio) {
+        this.inicio = inicio;
+    }
+
+    public void setFim(Calendar fim) {
+        this.fim = fim;
+    }
+
+    public void setEpoca(String epoca) {
+        this.epoca = epoca;
+    }
+
+    public Calendar getInicio() {
+        return inicio;
+    }
+
+    public Calendar getFim() {
+        return fim;
+    }
+
+    public String getEpoca() {
+        return epoca;
+    }
+    
+    
     
 }
