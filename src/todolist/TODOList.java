dbe268808908e02230667c03fc3b23925286713e
Ricardo Espanhol <ricardo14mata@hotmail.com>
2016-11-25 14:57:22 +0000

    new acresenta ao master

--- src/todolist/TODOList.java
+++ src/todolist/TODOList.java
@@ -17,7 +17,7 @@ public class TODOList {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Master");
-        System.out.println("Acresenta ao Master");
+        System.out.println("Acresenta ao Master\n");
     }
     
 }
