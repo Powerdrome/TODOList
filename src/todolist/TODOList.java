75792a3dfd2b4c30bec9373c32e01390f941fb39
Ricardo Espanhol <ricardo14mata@hotmail.com>
2016-11-25 15:40:20 +0000

    new adicionar linha

--- src/todolist/TODOList.java
+++ src/todolist/TODOList.java
@@ -17,7 +17,6 @@ public class TODOList {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World\n");
-        System.out.println("Hello World\n");
     }
     
 }
