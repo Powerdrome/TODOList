5355718ab7ff643dd8d59641f7dc8fd0c9a25308
Ricardo Espanhol <ricardo14mata@hotmail.com>
2016-11-25 15:26:27 +0000

    Merge origin/master into Ricardo
    Conflicts:
    	src/todolist/TODOList.java

--- src/todolist/TODOList.java
+++ src/todolist/TODOList.java
@@ -16,8 +16,7 @@ public class TODOList {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello Master");
-        System.out.println("Hello Master again");
+        
     }
     
 }
