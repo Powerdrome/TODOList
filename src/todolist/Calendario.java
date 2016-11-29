bd33c41799b44c68d43eeccafa3d9eca92f5fab1
JorgeCecilio <jorgetrovisco@10.65.129.97>
2016-11-29 22:09:08 +0000

    Comparador e correcção de nota

--- src/todolist/Calendario.java
+++ src/todolist/Calendario.java
@@ -1,6 +1,7 @@
 package todolist;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Calendario {
     private ArrayList<UnidadeCurricular> cadeiras;
@@ -19,6 +20,24 @@ public class Calendario {
     
     public void addCadeira(UnidadeCurricular cadeira){
         cadeiras.add(cadeira);
-        
+    }
+    
+    public ArrayList<Hora> getHoras(){
+        ArrayList<Hora> horas = new ArrayList<>();
+        String exame;
+        for(UnidadeCurricular uc : cadeiras){
+            for(HoraAula ha: (uc.getAulas())){
+                horas.add(new Hora(uc.getNome(), ha.getInicio(), ha.getFim()));
+            }
+            for(HoraEstudo he: (uc.getHorasEstudo())){
+                horas.add(new Hora(he.getTitulo(), he.getInicio(), he.getFim()));
+            }
+            for(Exame ex: (uc.getExames())){
+                exame = "exame: "+uc.getNome();
+                horas.add(new Hora(exame, ex.getInicio(), ex.getFim()));
+            }
+        }
+        Collections.sort(horas, new ComparadorHoras());
+        return horas;
     }
 }
