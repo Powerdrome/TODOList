8707ed1782785a008c9c1ad0747cabaaf0543631
JorgeCecilio <jorgetrovisco@10.65.129.97>
2016-12-02 02:41:57 +0000

    Mais cenas fixes a trabalhar

--- src/todolist/Dados.java
+++ src/todolist/Dados.java
@@ -41,6 +41,36 @@ public class Dados extends Observable{
         if(calendario == null) {
             calendario = new Calendario(); // faz o que tem a fazer para criar novo calendário
         }
+        criar_uc();
+    }
+    
+    private void criar_uc() {
+        calendario = new Calendario();
+        
+        UnidadeCurricular pa_uc = new UnidadeCurricular("Programação Distrubuída", 2, 1);
+        Nota pa_nota = new Nota("Programação Distribuida Nota", "Isto é uma nota para PA");
+        
+        Dica pa_dica = new Dica("Programação Distribuida Dica", "Isto é uma dica pra PA");
+       
+        Exame pa_exame = new Exame("Normal");
+        pa_exame.setInicio(2017,01,14,14,30);
+        pa_exame.setFim(2017,01,14,16,30);
+        
+        HoraAula pa_horaaula = new HoraAula(1, "L2.1");
+        pa_horaaula.setInicio(2016,01,14,17,30);
+        pa_horaaula.setFim(2016,01,14,18,00);
+        
+        HoraEstudo pa_horaestudo = new HoraEstudo("Hora d e Estudo de PA");
+        pa_horaestudo.setInicio(2017,01,13,17,30);
+        pa_horaestudo.setFim(2017,01,13,18,00);
+        
+        pa_uc.addAulas(pa_horaaula);
+        pa_uc.addDica(pa_dica);
+        pa_uc.addNota(pa_nota);
+        pa_uc.addExame(pa_exame);
+        pa_uc.addHoraEstudo(pa_horaestudo);
+        
+        calendario.addCadeira(pa_uc);
     }
     
     public Dados(Calendario calendario) {
