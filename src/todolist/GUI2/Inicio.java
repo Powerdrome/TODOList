5b717a7fb31b95a747a099c3293e08780be76a32
JorgeCecilio <jorgetrovisco@10.65.129.97>
2016-12-02 02:25:43 +0000

    Resolução de problemas

--- src/todolist/GUI2/Inicio.java
+++ src/todolist/GUI2/Inicio.java
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package todolist.GUI2;
-
-import java.awt.*;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class Inicio extends JPanel {
-
-   public static void main(String[] args) {
-      
-
-      @SuppressWarnings("unchecked")
-      JFrame frame = new JFrame();
-      
-      frame.setLayout(new BorderLayout());
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-      
-      frame.add(new Calendario(), BorderLayout.CENTER);
-      frame.add(new BarraLateral(), BorderLayout.WEST);
-      frame.add(new BarraSuperior(), BorderLayout.NORTH);
-      
-
-      frame.pack();
-      frame.setSize(1200, 700);
-      frame.setMinimumSize(new Dimension(1000, 500));
-      frame.setMaximumSize(new Dimension(1000,500));
-      frame.setPreferredSize(new Dimension(1000,500));
-      frame.setVisible(true);
-      
-
-
-   }
-=======
-=======
->>>>>>> origin/Ricardo
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package todolist.GUI2;
 
 import java.awt.*;
@@ -62,16 +12,17 @@ import todolist.Nota;
 import todolist.UnidadeCurricular;
 
 public class Inicio extends JPanel {
-   
    public static void main(String[] args) {
-      
+        int ver = 2; //1->Calendario 2->Tarefas
         
         @SuppressWarnings("unchecked")
         JFrame frame = new JFrame();
         frame.setLayout(new BorderLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        frame.add(new Calendario(), BorderLayout.CENTER);
+        if(ver == 1)
+            frame.add(new Calendario(), BorderLayout.CENTER);
+        else if(ver == 2)
+            frame.add(new Tarefas(), BorderLayout.CENTER);
         frame.add(new BarraLateral(), BorderLayout.WEST);
         frame.add(new BarraSuperior(), BorderLayout.NORTH);
       
@@ -122,51 +73,4 @@ public class Inicio extends JPanel {
         
         calendario.addCadeira(pa_uc);
     }
-<<<<<<< HEAD
->>>>>>> origin/Ricardo
-=======
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package todolist.GUI2;
-
-import java.awt.*;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class Inicio extends JPanel {
-   
-   public static void main(String[] args) {
-        int ver = 2; //1->Calendario 2->Tarefas
-        @SuppressWarnings("unchecked")
-        JFrame frame = new JFrame();
-        frame.setLayout(new BorderLayout());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        if(ver == 1)
-            frame.add(new Calendario(), BorderLayout.CENTER);
-        else if(ver == 2)
-            frame.add(new Tarefas(), BorderLayout.CENTER);
-            
-        frame.add(new BarraLateral2(), BorderLayout.WEST);
-        frame.add(new BarraSuperior(), BorderLayout.NORTH);
-      
-        frame.pack();
-        int x = 870, y=600;
-        frame.setSize(x, y);
-        frame.setVisible(true);
-        frame.getContentPane().setBackground(Color.GRAY);
-        if(System.getProperty("os.name").contains("Windows")) {
-            frame.setResizable(false);
-        }
-        else {
-            frame.setMinimumSize(new Dimension(x, y));
-            frame.setMaximumSize(new Dimension(x, y));
-        }
-   }
->>>>>>> refs/remotes/origin/master
->>>>>>> origin/Ricardo
 }
