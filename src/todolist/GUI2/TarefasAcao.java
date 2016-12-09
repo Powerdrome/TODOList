4bdf34f72298024b96f1b9e2634a553d93eb1fe2
JorgeTrovisco <jorgetrovisco@MacBook-Pro-de-Jorge-2.local>
2016-12-09 14:43:49 +0000

    TestesCalendario

--- src/todolist/GUI2/TarefasAcao.java
+++ src/todolist/GUI2/TarefasAcao.java
@@ -20,17 +20,12 @@ public class TarefasAcao extends JDialog {
     public TarefasAcao(Dados dados) {
         this.dados=dados;
         setLayout(new BorderLayout());
-        getContentPane().setBackground(Color.BLUE);
         setSize(new Dimension(400, 70));
         setLocation(200,200);
         setTitle("Selecionar ação");
         createAndDisplay();
         setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
         setVisible(true);
-//        botaoPassar = new JButton();
-//        botaoPassar.setText("Passar");
-//        botaoPassar.setAlignmentX(Component.CENTER_ALIGNMENT); 
-//        botaoPassar.setVisible(false);
     }
     protected void createAndDisplay(){
         JPanel frame = new JPanel(new GridLayout(0, 2));
